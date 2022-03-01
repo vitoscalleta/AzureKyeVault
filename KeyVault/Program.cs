eec9ae5fcb8503a976cfb25e99cd5d672132eefe
Hari <infy_723012@outlook.com>
2022-03-01 20:33:13 +0530

    Keyvault 2

--- KeyVault/Program.cs
+++ KeyVault/Program.cs
@@ -8,47 +8,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        string secretName = ConfigurationManager.AppSettings["SecretName"];
-        string keyVaultName = "LearnAzKeyVaultHari";
-        var kvUri = "https://learnazkeyvaulthari.vault.azure.net/";
-        SecretClientOptions options = new SecretClientOptions()
+        try
         {
-            Retry =
+            string secretName = ConfigurationManager.AppSettings["SecretName"];
+            string keyVaultName = "LearnAzKeyVaultHari";
+            var kvUri = "https://connectionstringhari.vault.azure.net/";
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
                 {
                     Delay= TimeSpan.FromSeconds(2),
                     MaxDelay = TimeSpan.FromSeconds(16),
                     MaxRetries = 5,
                     Mode = RetryMode.Exponential
                  }
-        };
+            };
 
-        var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(), options);
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(), options);
 
-        Console.Write("Input the value of your secret > ");
-        string secretValue = Console.ReadLine();
+            Console.Write("Input the value of your secret > ");
+            string secretValue = Console.ReadLine();
 
-        Console.Write("Creating a secret in " + keyVaultName + " called '" + secretName + "' with the value '" + secretValue + "` ...");
+            Console.Write("Creating a secret in " + keyVaultName + " called '" + secretName + "' with the value '" + secretValue + "` ...");
 
-        client.SetSecret(secretName, secretValue);
+            client.SetSecret(secretName, secretValue);
 
-        Console.WriteLine(" done.");
+            Console.WriteLine(" done.");
 
-        Console.WriteLine("Forgetting your secret.");
-        secretValue = "";
-        Console.WriteLine("Your secret is '" + secretValue + "'.");
+            Console.WriteLine("Forgetting your secret.");
+            secretValue = "";
+            Console.WriteLine("Your secret is '" + secretValue + "'.");
 
-        Console.WriteLine("Retrieving your secret from " + keyVaultName + ".");
+            Console.WriteLine("Retrieving your secret from " + keyVaultName + ".");
 
-        KeyVaultSecret secret = client.GetSecret(secretName);
+            KeyVaultSecret secret = client.GetSecret(secretName);
 
-        Console.WriteLine("Your secret is '" + secret.Value + "'.");
+            Console.WriteLine("Your secret is '" + secret.Value + "'.");
 
-        Console.Write("Deleting your secret from " + keyVaultName + " ...");
+            Console.Write("Deleting your secret from " + keyVaultName + " ...");
 
-        client.StartDeleteSecret(secretName);
+            client.StartDeleteSecret(secretName);
 
-        System.Threading.Thread.Sleep(5000);
-        Console.WriteLine(" done.");
+            System.Threading.Thread.Sleep(5000);
+            Console.Beep();
+            Console.Beep(37,3000);
 
+            Console.WriteLine(" done.");
+            Console.ReadLine();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            Console.ReadLine();
+        }
     }
 }
